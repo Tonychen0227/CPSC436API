a1f84e8e8461794d2db4437b45138a66f9511730
Tony Chen <tony.chen@outlook.com>
2019-06-28 19:49:43 -0700

    Initial commit

---
+++ mongo/users.js
@@ -0,0 +1,87 @@
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://admin:admin@cpsc436-basketball-shard-00-00-kbwxu.mongodb.net:27017,cpsc436-basketball-shard-00-01-kbwxu.mongodb.net:27017,cpsc436-basketball-shard-00-02-kbwxu.mongodb.net:27017/test?ssl=true&replicaSet=CPSC436-Basketball-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  database = db
+
+  usersCollection = db.collection('users')
+});
+
+module.exports = {}
+
+module.exports.getUsers = function() {
+  return new Promise(function(resolve, reject) {
+    MongoClient.connect(url, function(err, db) {
+      assert.equal(null, err);
+
+      const usersCollection = db.collection('users')
+      usersCollection.find({}).toArray(function(err, docs) {
+        if (err != null) {
+          reject(err)
+        }
+        resolve(docs)
+      });
+    });
+  });
+}
+
+module.exports.insertUser = function(user) {
+  return new Promise(function(resolve, reject) {
+    MongoClient.connect(url, function(err, db) {
+      assert.equal(null, err);
+      const usersCollection = db.collection('users')
+      usersCollection.insert(user, function(err, docs) {
+        if (err != null) {
+          reject(err)
+        }
+        resolve(docs)
+      });
+    });
+  });
+}
+
+module.exports.updateOneUserJwt = function(id, jwt, jwtIssued) {
+  return new Promise(function(resolve, reject) {
+    MongoClient.connect(url, function(err, db) {
+      assert.equal(null, err);
+      const usersCollection = db.collection('users')
+      usersCollection.updateOne({_id: id}, {$set: {
+        "JWTToken": jwt,
+        "JWTIssued": jwtIssued
+      }}, function(err, docs) {
+        if (err != null) {
+          reject(err)
+        }
+        console.log("Found the following records");
+        console.log(docs)
+        resolve(docs)
+      });
+    });
+  });
+}
+
+module.exports.validateOneUser = function(id) {
+  return new Promise(function(resolve, reject) {
+    MongoClient.connect(url, function(err, db) {
+      assert.equal(null, err);
+      const usersCollection = db.collection('users')
+      usersCollection.updateOne({_id: id}, {$set: {
+        "Validated": true
+      }}, function(err, docs) {
+        if (err != null) {
+          reject(err)
+        }
+        console.log("Found the following records");
+        console.log(docs)
+        resolve(docs)
+      });
+    });
+  });
+}
---
+++ routes/users.js
@@ -0,0 +1,190 @@
+var express = require('express');
+var router = express.Router();
+const shortid = require('shortid');
+let jwt = require('jsonwebtoken');
+const Users = require('../mongo/users');
+const config = require('../config.js');
+const sendinblue = require('sib-api-v3-sdk');
+
+var mailClient = sendinblue.ApiClient.instance;
+var apiKey = mailClient.authentications['api-key'];
+apiKey.apiKey = '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';
+
+var apiInstance = new sendinblue.SMTPApi();
+
+var sendSmtpEmail = new sendinblue.SendSmtpEmail(); // SendSmtpEmail | Values to send a transactional email
+
+var htmlContentRoot = "<h3>Please click this link to confirm your verification for GGPanda!</h3> <br/> <a href='http://localhost:3001/users?validate="
+
+var htmlContentTail = "' target='_blank'>Click here! This is not scam I swear </a>"
+
+sendSmtpEmail.sender = { "email":"tony.chen@outlook.com", "name":"GGPanda Basketball"};
+sendSmtpEmail.subject = "CPSC436 Basketball: Account Verification";
+
+const sendValidationEmail = (email, token) => {
+  sendSmtpEmail.to = [{"email": email}]
+  console.log(sendSmtpEmail)
+  sendSmtpEmail.htmlContent = htmlContentRoot + token + htmlContentTail;
+  apiInstance.sendTransacEmail(sendSmtpEmail).then(function(data) {
+    console.log('API called successfully. Returned data: ' + data);
+  }, function(error) {
+    console.error(error);
+  });
+}
+
+router.get('/', function(req, res, next){
+  if (req.query.validate) {
+    Users.getUsers().then(success => {
+      users = success
+      for (var x = 0; x < users.length; x++) {
+        if (users[x].ValidationToken == req.query.validate) {
+          Users.validateOneUser(users[x]._id).then(success => {
+            res.json("Validation successful")
+          }).catch(err => {
+            throw new Error(err)
+          })
+        }
+      }
+    }).catch(err => {
+      res.statusCode = 401;
+      res.send(err.toString());
+    })
+  } else {
+    res.status = 401;
+    res.send("Unauthorized")
+  }
+});
+
+const handleUsernamePasswordLogin = (users, email, password) => {
+  // password should already be hashed
+  for (var x = 0; x < users.length; x++) {
+    if (users[x].email == email) {
+      if (users[x].password == password) {
+        if (!users[x].Validated) {
+          sendValidationEmail(users[x].email, users[x].ValidationToken)
+          throw new Error("Verification email resent")
+        }
+        let token = jwt.sign({email: email},
+          config.secret,
+          { expiresIn: '24h' // expires in 24 hours
+          }
+        );
+        users[x].JWTToken = token
+        users[x].JWTIssued = new Date().toUTCString()
+        Users.updateOneUserJwt(users[x]._id, users[x].JWTToken, users[x].JWTIssued).then(success => {
+          Users.getUsers().then(succ => {
+            for (var x = 0; x < succ.length; x++) {
+              if (succ[x].email == email) {
+                return succ[x]
+              }
+            }
+          }).catch(err => {
+            throw new Error(err)
+          })
+        }).catch(err => {
+          next(err)
+        })
+        return users[x]
+      } else {
+        throw new Error("Unauthorized (incorrect email/password)")
+      }
+    }
+  }
+  throw new Error("No corresponding user email found");
+}
+
+const handleJWTLogin = (users, token) => {
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      throw new Error("Invalid token")
+    }
+  });
+  for (var x = 0; x < users.length; x++) {
+    if (users[x].JWTToken == token) {
+      let date = new Date(users[x].JWTIssued)
+      if (new Date() <= date.setDate(date.getDate() + 1)) {
+        return users[x]
+      } else {
+        throw new Error ("Token expired")
+      }
+    }
+  }
+  throw new Error("No corresponding JWT found")
+}
+
+router.post('/login', function(req, res, next) {
+  Users.getUsers().then(success => {
+    users = success
+    if (req.body.email && req.body.password) {
+      var result = handleUsernamePasswordLogin(users, req.body.email, req.body.password)
+      res.json(result)
+    }
+    else if (req.body.jwt) {
+      var result = handleJWTLogin(users, req.body.jwt)
+      res.json(result)
+    } else {
+      throw new Error("No authentication supplied")
+    }
+  }).catch(err => {
+    res.statusCode = 401;
+    res.send(err.toString());
+  })
+})
+
+const checkRegUser = (users, email, password) => {
+  if (!email || !password) {
+    throw new Error("Forgot email or password")
+  }
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var match = re.test(email)
+  if (!match) {
+    throw new Error("Invalid email")
+  }
+  if (password.length < 8) {
+    throw new Error("Password is too short")
+  }
+  for (var x = 0; x < users.length; x++) {
+    if (users[x]["email"] == email) {
+      throw new Error("Email is taken")
+    }
+  }
+  let token = jwt.sign({username: email},
+    config.secret,
+    { expiresIn: '24h' // expires in 24 hours
+    }
+  );
+  let newDate = new Date()
+  var userJson = {
+    "email": email,
+    "password": password,
+    "JWTToken": token,
+    "JWTIssued": newDate.toUTCString(),
+    "FavoriteTeam": "",
+    "AccountCreated": new Date().toLocaleDateString(),
+    "SpecialPermissions": "",
+    "ValidationToken": shortid.generate(),
+    "Validated": false
+  }
+  //TODO: send verification email
+  return userJson;
+}
+
+router.post('/register', function(req, res, next) {
+  Users.getUsers().then(success => {
+    users = success
+    var userJSON = checkRegUser(users, req.body.email, req.body.password);
+    Users.insertUser(userJSON).then(success => {
+      console.log(success.ops[0]);
+      sendValidationEmail(success.ops[0].email, success.ops[0].ValidationToken);
+      throw new Error("User registered. Please verify email.")
+    }).catch(err => {
+      res.statusCode = 500;
+      res.send(err.toString());
+    })
+  }).catch(err => {
+    res.statusCode = 403;
+    res.send(err.toString());
+  })
+})
+
+module.exports = router;
