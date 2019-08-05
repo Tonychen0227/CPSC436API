6576d0c4ca1027edb8f2a4a7a31c4e0b4c479962
Tony Chen <tony.chen@outlook.com>
2019-08-05 15:45:56 -0700

    API completed for discussions

--- mongo/posts.js
+++ mongo/posts.js
@@ -1,5 +1,6 @@
 var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
+const {ObjectId} = require('mongodb');
 
 var shortid = require('shortid');
 const config = require('../config.js');
@@ -40,42 +41,150 @@ module.exports.getPosts = function() {
   });
 }
 
-module.exports.insertPost = function(post) {
+module.exports.getPost = function(id) {
   return new Promise(function(resolve, reject) {
     MongoClient.connect(url, function(err, db) {
       assert.equal(null, err);
+
       const postsCollection = db.collection('posts');
-      postsCollection.insert(post, function(err, docs) {
+      postsCollection.find({}).toArray(function(err, docs) {
         if (err != null) {
           reject(err);
           db.close();
         }
-        resolve(docs);
+        resolve(docs.find(x => x._id == id));
         db.close();
       });
     });
   });
 }
 
-module.exports.insertComment = function(id, body, userId, existingComments) {
+module.exports.insertPost = function(postTitle, postBody, userId, userName) {
   return new Promise(function(resolve, reject) {
     MongoClient.connect(url, function(err, db) {
       assert.equal(null, err);
       const postsCollection = db.collection('posts');
-      existingComments.push({
+      post = {
+        "userId": userId,
+        "userName": userName,
+        "postTitle": postTitle,
+        "postBody": postBody,
+        "comments": [],
+        "postedDate": new Date(Date.now()).toLocaleString()
+      }
+      postsCollection.insert(post, function(err, docs) {
+        if (err != null) {
+          reject(err);
+          db.close();
+        }
+        postsCollection.find({}).toArray(function(err, docs) {
+          resolve(docs);
+        });
+        db.close();
+      });
+    });
+  });
+}
+
+module.exports.insertComment = function(id, body, userId, userName) {
+  return new Promise(function(resolve, reject) {
+    MongoClient.connect(url, function(err, db) {
+      assert.equal(null, err);
+      const postsCollection = db.collection('posts');
+      const newComment = ({
+        "id": shortid.generate(),
         "content": body,
         "userId": userId,
-        "postedDate": new Date(Date.now()).toLocaleString();
-      })
-      postsCollection.updateOne({_id: id}, {$set: {
-        "comments": existingComments
+        "userName": userName,
+        "postedDate": new Date(Date.now()).toLocaleString()
+      });
+      postsCollection.updateOne({_id: ObjectId(id)}, {$push: {
+        "comments": newComment
       }}, function(err, docs) {
         if (err != null) {
           reject(err);
           db.close();
         }
-        resolve(docs);
+        postsCollection.find({_id: ObjectId(id)}).toArray(function(err, docs) {
+          resolve(docs[0]);
+        });
         db.close();
+      });
+    });
+  });
+}
+
+module.exports.editComment = function(id, commentId, body, userId) {
+  return new Promise(function(resolve, reject) {
+    MongoClient.connect(url, function(err, db) {
+      assert.equal(null, err);
+      const postsCollection = db.collection('posts');
+      postsCollection.find({_id: ObjectId(id)}).toArray(function(err, docs) {
+        if (err != null || docs.length == 0) {
+          reject("Error encountered or nothing found");
+          db.close();
+        }
+        let existingComments = docs[0].comments;
+        const foundIndex = existingComments.findIndex(x => x.id == commentId);
+        try {
+          if (existingComments[foundIndex].userId.toString() != userId.toString()) {
+            reject("Not your comment");
+          }
+          existingComments[foundIndex].content = body;
+          existingComments[foundIndex].editedDate = new Date(Date.now()).toLocaleString();
+        } catch {
+          reject("Comment not found")
+        }
+        postsCollection.updateOne({_id: ObjectId(id)}, {$set: {
+          "comments": existingComments
+        }}, function(err, docs) {
+          if (err != null) {
+            reject(err);
+            db.close();
+          }
+          postsCollection.find({_id: ObjectId(id)}).toArray(function(err, docs) {
+            console.log(err);
+            resolve(docs[0]);
+            db.close();
+          });
+        });
+      });
+    });
+  });
+}
+
+module.exports.deleteComment = function(id, commentId, body, userId) {
+  return new Promise(function(resolve, reject) {
+    MongoClient.connect(url, function(err, db) {
+      assert.equal(null, err);
+      const postsCollection = db.collection('posts');
+      postsCollection.find({_id: ObjectId(id)}).toArray(function(err, docs) {
+        if (err != null || docs.length == 0) {
+          reject(err);
+          db.close();
+        }
+        let existingComments = docs[0].comments;
+        const foundIndex = existingComments.findIndex(x => x.id == commentId);
+        try {
+          if (existingComments[foundIndex].userId.toString() != userId.toString()) {
+            reject("Not your comment");
+          }
+        } catch {
+          reject("Comment not found")
+        }
+        existingComments.splice(foundIndex, 1);
+        postsCollection.updateOne({_id: ObjectId(id)}, {$set: {
+          "comments": existingComments
+        }}, function(err, docs) {
+          if (err != null) {
+            reject(err);
+            db.close();
+          }
+          postsCollection.find({_id: ObjectId(id)}).toArray(function(err, docs) {
+            resolve(docs[0]);
+            db.close();
+          });
+        });
       });
     });
   });
--- routes/discussion.js
+++ routes/discussion.js
@@ -3,151 +3,36 @@ var router = express.Router();
 const shortid = require('shortid');
 let jwt = require('jsonwebtoken');
 const Posts = require('../mongo/posts');
+const Users = require('../mongo/users')
 const config = require('../config.js');
 const axios = require('axios');
 const fetch = require('fetch-base64');
 const image2base64 = require('image-to-base64');
 
 router.get('/', function(req, res, next){
-  if (req.query.validate) {
-    Users.getUsers().then(success => {
-      users = success;
-      let user = users.filter((function(item)  {
-        return item.ValidationToken == req.query.validate;
-      }))
-      user = user[0];
-      if (user) {
-        Users.validateOneUser(user._id).then(success => {
-          res.json("Validation successful");
-        }).catch(err => {
-          throw new Error(err);
-        })
-      }
-    }).catch(err => {
-      res.statusCode = 401;
-      res.send(err.toString());
-    })
-  } else if (req.query.reset) {
-    Users.getUsers().then(success => {
-      users = success;
-      let sent = false;
-      let user = users.filter(function(item) {
-        return item.ResetToken == req.query.reset && item.Password != "Facebook";
-      })
-      user = user[0];
-      if (user) {
-        Users.resetPWOneUser(user._id).then(success => {
-          res.json("Reset successful, please log in with new password and it will set.")
-        }).catch(err => {
-          throw new Error(err);
-        })
-      } else {
-        res.statusCode = 401;
-        res.send("Invalid reset provided");
-      }
-    }).catch(err => {
-      res.statusCode = 401;
-      res.send(err.toString());
-    })
-  }
-  else {
-    res.status = 401;
-    res.send("Unauthorized");
-  }
+  Posts.getPosts().then(success => {
+    res.json(success);
+  }).catch(err => {
+    res.statusCode = 401;
+    res.send(err.toString());
+  })
 });
 
-const handleUsernamePasswordLogin = (users, email, password) => {
-  // password should already be hashed
-  return new Promise(function(resolve, reject) {
-    let user = users.filter(function(item) {
-      return item.Email == email;
-    })
-    user = user[0];
-    if (user) {
-      if (user.Password == password) {
-        if (!user.Validated) {
-          sendValidationEmail(user.Email, user.ValidationToken);
-          reject("Verification email resent");
-        }
-        let token = jwt.sign({email: email},
-          config.secret,
-          { expiresIn: '24h' // expires in 24 hours
-          }
-        );
-        user.JWTToken = token;
-        user.JWTIssued = new Date().toUTCString();
-        Users.updateOneUserJwt(user._id, user.JWTToken, user.JWTIssued).then(success => {
-          Users.getUsers().then(succ => {
-            let user = succ.filter(function(item) {
-              return item.Email == email;
-            })
-            user = user[0];
-            if (user) {
-              resolve(user);
-            } else {
-              reject("???");
-            }
-          }).catch(err => {
-            reject(err);
-          })
-        }).catch(err => {
-          reject(err);
-        })
-        resolve(user);
-      } else if (user.Password == "") {
-        Users.setPWOneUser(user._id, password).then(succ1 => {
-          Users.getUsers().then(succ => {
-            let user = succ.filter(function(item) {
-              return item.Email == email;
-            })
-            user = user[0];
-            if (user) {
-              resolve(user);
-            } else {
-              reject("???");
-            }
-          }).catch(err => {
-            reject(err);
-          })
-        }).catch(err => {
-          reject(err);
-        })
-      } else {
-        reject("Unauthorized (incorrect email/password). Is this a Facebook account?");
-      }
-    } else {
-      reject("No corres. email found");
-    }
-  })
-}
-
-const handleJWTLogin = (users, token) => {
-  jwt.verify(token, config.secret, (err, decoded) => {
-    if (err) {
-      throw new Error("Invalid token");
-    }
-  });
-  let user = users.filter(function(item) {
-    return item.JWTToken == token;
-  }) 
-  user = user[0];
-  if (user) {
-    let date = new Date(user.JWTIssued);
-    if (new Date() <= date.setDate(date.getDate() + 1)) {
-      return user;
-    } else {
-      throw new Error("Token expired");
-    }
-  } else {
-    throw new Error("No corresponding JWT found");
-  }
-}
-
-router.post('/login', function(req, res, next) {
+router.post('/', function(req, res, next) {
   Users.getUsers().then(success => {
     users = success;
-    if (req.body.email && req.body.password) {
-      handleUsernamePasswordLogin(users, req.body.email, req.body.password)
+    if (!req.body) {
+      throw new Error("No body supplied");
+    }
+    if (!req.body.title || req.body.title.length < 10) {
+      throw new Error("No title supplied or title is less than 10 characters");
+    }
+    if (!req.body.body) {
+      req.body.body = "";
+    }
+    if (req.body.token) {
+      const user = users.filter(x => x.JWTToken == req.body.token)[0];
+      Posts.insertPost(req.body.title, req.body.body, user._id, user.DisplayName)
       .then(succ => {
         res.json(succ);
       })
@@ -156,10 +41,7 @@ router.post('/login', function(req, res, next) {
         res.send(oops.toString());
       });
     }
-    else if (req.body.jwt) {
-      let result = handleJWTLogin(users, req.body.jwt);
-      res.json(result);
-    } else {
+    else {
       throw new Error("No authentication supplied");
     }
   }).catch(err => {
@@ -168,136 +50,37 @@ router.post('/login', function(req, res, next) {
   })
 })
 
-const checkRegUser = (users, email, password) => {
-  if (!email || !password) {
-    throw new Error("Forgot email or password");
-  }
-  let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  let match = re.test(email);
-  if (!match) {
-    throw new Error("Invalid email");
-  }
-  if (password.length < 8) {
-    throw new Error("Password is too short");
-  }
-  let user = users.filter(function(item) {
-    return item.Email == email;
-  })
-  user = user[0];
-  if (user) {
-    throw new Error("Email is taken");
-  }
-  let token = jwt.sign({username: email},
-    config.secret,
-    { expiresIn: '24h' // expires in 24 hours
-    }
-  );
-  let newDate = new Date();
-  
-  let userJson = {
-    "Email": email,
-    "Password": password,
-    "JWTToken": token,
-    "JWTIssued": newDate.toUTCString(),
-    "FavoriteTeam": "",
-    "AccountCreated": new Date().toLocaleDateString(),
-    "SpecialPermissions": "",
-    "ValidationToken": shortid.generate(),
-    "Validated": false,
-    "ResetToken": shortid.generate(),
-    "DisplayName": "I love JavaScript XD",
-    "ProfileBase64": ""
-  };
-  return userJson;
-}
-
-router.post('/register', function(req, res, next) {
-  Users.getUsers().then(success => {
-    users = success;
-    let userJSON = checkRegUser(users, req.body.email, req.body.password);
-    userJSON.DisplayName = req.body.displayName != '' ? req.body.displayName : userJSON.DisplayName;
-    userJSON.FavoriteTeam = req.body.favTeam;
-    fetch.remote(defaultImage).then((data) => {
-      userJSON.ProfileBase64 = data[1].split(',')[1];
-      Users.insertUser(userJSON).then(success => {
-        sendValidationEmail(success.ops[0].Email, success.ops[0].ValidationToken);
-        throw new Error("User registered. Please verify email.");
-      }).catch(err => {
-        res.statusCode = 500;
-        res.send(err.toString());
-      })
-    }).catch((reason) => {throw new Error(reason)});
+router.get('/:id', function(req, res, next) {
+  Posts.getPost(req.params.id).then(success => {
+    res.json(success);
   }).catch(err => {
-    res.statusCode = 403;
+    res.statusCode = 401;
     res.send(err.toString());
   })
 })
 
-router.post('/reset', function(req, res, next) {
+router.post('/:id', function(req, res, next) {
   Users.getUsers().then(success => {
     users = success;
-    if (req.body.email) {
-      let user = users.filter(function(item) {
-        return item.Email == req.body.email && item.Password != "Facebook";
-      })
-      user = user[0];
-      if (user) {
-          if (user.Validated) {
-            Users.updateResetTokenOneUser(user._id).then(succ => {
-              sendResetEmail(req.body.email, succ);
-              res.statusCode = 500;
-              res.send("Email sent!");
-            }).catch(err => {
-              console.log(err);
-              throw new Error(err);
-            })
-          } else {
-            sendValidationEmail(user.Email, user.ValidationToken);
-            res.statusCode = 401;
-            res.send("Cannot reset password on an invalidated account. Validation email resent.");
-          }
-      } else {
-          res.statusCode = 400;
-          res.send("Email not found :( Or account is Facebook");
-        }
-    } else {
-      throw new Error("No Email provided");
+    if (!req.body) {
+      throw new Error("No body supplied");
     }
-  }).catch(err => {
-    res.statusCode = 400;
-    res.send(err.toString());
-  })
-})
-
-router.post('/updateDisplay', function(req, res, next) {
-  if (!req.body || !req.body.email || !req.body.password || !req.body.name || !req.body.team) {
-    res.statusCode = 500;
-    res.send("You must include email, password, name, team");
-    return;
-  }
-  if (req.body.name.length < 2 || req.body.name.length > 30) {
-    res.statusCode = 500;
-    res.send("Bad name");
-    return;
-  }
-  Users.getUsers().then(success => {
-    users = success;
-    let user = users.filter(function(item) {
-      return item.Email == req.body.email && item.Password == req.body.password;
-    })
-    if (user) {
-      user = user[0];
-      Users.setNameTeamOneUser(user._id, req.body.name, req.body.team).then(success => {
-          res.json(success);
-        }).catch(err => {
-          res.statusCode = 500;
-          res.send(err.toString());
-          return;
+    if (!req.body.body || req.body.body.length < 10) {
+      throw new Error("No comment body supplied or title is less than 10 characters");
+    }
+    if (req.body.token) {
+      const user = users.filter(x => x.JWTToken == req.body.token)[0];
+      Posts.insertComment(req.params.id, req.body.body, user._id, user.DisplayName)
+      .then(succ => {
+        res.json(succ);
       })
-    } else {
-      res.statusCode = 401;
-      res.send("No valid user");
-      return;
+      .catch(oops => {
+        res.statusCode = 401;
+        res.send(oops.toString());
+      });
+    }
+    else {
+      throw new Error("No authentication supplied");
     }
   }).catch(err => {
     res.statusCode = 401;
@@ -305,30 +88,28 @@ router.post('/updateDisplay', function(req, res, next) {
   })
 })
 
-router.post('/uploadProfile', function(req, res, next) {
-  if (!req.body || !req.body.email || !req.body.password || !req.body.base64) {
-    res.statusCode = 500;
-    res.send("You must include email, password, base64");
-    return;
-  }
+router.patch('/:postId/comment/:commentId', function(req, res, next) {
   Users.getUsers().then(success => {
     users = success;
-    let user = users.filter(function(item) {
-      return item.Email == req.body.email && item.Password == req.body.password;
-    })
-    if (user) {
-      user = user[0];
-      Users.setBase64OneUser(user._id, req.body.base64).then(success => {
-          res.json(success);
-        }).catch(err => {
-          res.statusCode = 500;
-          res.send(err.toString());
-          return;
+    if (!req.body) {
+      throw new Error("No body supplied");
+    }
+    if (!req.body.body || req.body.body.length < 10) {
+      throw new Error("No comment body supplied or title is less than 10 characters");
+    }
+    if (req.body.token) {
+      const user = users.filter(x => x.JWTToken == req.body.token)[0];
+      Posts.editComment(req.params.postId, req.params.commentId, req.body.body, user._id)
+      .then(succ => {
+        res.json(succ);
       })
-    } else {
-      res.statusCode = 401;
-      res.send("No valid user");
-      return;
+      .catch(oops => {
+        res.statusCode = 401;
+        res.send(oops.toString());
+      });
+    }
+    else {
+      throw new Error("No authentication supplied");
     }
   }).catch(err => {
     res.statusCode = 401;
@@ -336,74 +117,30 @@ router.post('/uploadProfile', function(req, res, next) {
   })
 })
 
-router.post('/fbLogin', function(req, res, next) {
-  console.log(req.body);
-  console.log(req.headers);
-  if (!req.body.email || !req.body.id || !req.body.token) {
-    res.statusCode = 400;
-    res.send("You must include email, id, token")
-  } else {
-    Users.getUsers().then(success => {
-      users = success;
-      let user = users.filter(function(item) {
-        return item.Email == req.body.email && item.Password != "Facebook";
+router.delete('/:postId/comment/:commentId', function(req, res, next) {
+  Users.getUsers().then(success => {
+    users = success;
+    if (!req.body) {
+      throw new Error("No body supplied");
+    }
+    if (req.body.token) {
+      const user = users.filter(x => x.JWTToken == req.body.token)[0];
+      Posts.deleteComment(req.params.postId, req.params.commentId, req.body.body, user._id)
+      .then(succ => {
+        res.json(succ);
       })
-      user = user[0];
-      let userEmail = users.filter(function(item) {
-        return item.Email == req.body.email;
-      })
-      userEmail = userEmail[0];
-      if (user != undefined) {
-        res.statusCode = 500;
-        res.send("You already have an account with same email.");
-        return;
-      } else if (userEmail != undefined) {
-        res.json(userEmail);
-        return;
-      }
-      let newUser = {
-        "Email": req.body.email,
-        "Password": "Facebook",
-        "JWTToken": "No token for facebook users",
-        "JWTIssued": new Date().toUTCString(),
-        "FavoriteTeam": "raptors",
-        "AccountCreated": new Date().toLocaleDateString(),
-        "SpecialPermissions": {"FBID":  req.body.id},
-        "ValidationToken": shortid.generate(),
-        "Validated": true,
-        "ResetToken": shortid.generate(),
-        "DisplayName": "I love JavaScript XD",
-        "ProfileBase64": ""
-      };
-      axios.get(FBApi + req.body.id + '/?access_token=' + req.body.token)
-      .then(response => {
-        newUser.DisplayName = response.data.name;
-        axios.get(FBApi + req.body.id + FBApiPictureSuffix)
-        .then(response => {
-          image2base64(response.data.data.url)
-          .then(result => {
-            newUser.ProfileBase64 = result
-            Users.insertUser(newUser).then(succ => {
-              res.json(newUser);
-            }).catch(err => {
-              throw new Error(err);
-            })
-          }).catch(err => {
-            throw new Error(err);
-          })
-        })
-        .catch(err => {
-          throw new Error(err);
-        })
-      })
-      .catch(err => {
-        throw new Error(err);
-      })
-    }).catch(err => {
-      res.statusCode = 401;
-      res.send(err.toString());
-    })
-  }
+      .catch(oops => {
+        res.statusCode = 401;
+        res.send(oops.toString());
+      });
+    }
+    else {
+      throw new Error("No authentication supplied");
+    }
+  }).catch(err => {
+    res.statusCode = 401;
+    res.send(err.toString());
+  })
 })
 
 module.exports = router;
